19686475e123da4da3a17a9f3e419e8f9f63b314
Advyth Ashok <advythashok@gmail.com>
2019-06-17 19:49:44 +0530

    Code cleanup complete

--- Views/Common/ViewPost.js
+++ Views/Common/ViewPost.js
@@ -9,170 +9,177 @@ const SCREEN_WIDTH = Dimensions.get('window').width
 
 class ViewPost extends React.Component
 {
-    constructor(props)
-    {
-        super(props);
-        this.state = {
-            heading : "heading",
-            content : "content",
-            apiData : [],
-            apiDataComments : [],
-            commentsLoaded : false,
-            isRefreshing : false,
-            postTitle : "",
-            postAuthor : "",
-            goldAward : 0,
-            silverAward : 0,
-            platinumAward : 0,
-            score : 0,
-            url : "",
-            type : "",
-            link : "",
-            selftext : "",
-            nsfw : null,
-        }
-        this.renderComments  = this.renderComments.bind(this);
-        this.fetchSinglePost = this.fetchSinglePost.bind(this);
-        this.renderAcitivityIndicator = this.renderAcitivityIndicator.bind(this);
-        this.refresh = this.refresh.bind(this);
-        this.navigateToUrl = this.navigateToUrl.bind(this);
-    }
-    navigateToUrl(url, type, name)
-    {
-        const {navigation} = this.props;
-        this.props.navigation.navigate('webPage',{
-            url : url,
-            type : type,
-            name : name
-          });
-    }
-    async componentWillMount()
-    {
-        const {navigation} = this.props;
-        await this.setState({
-            postTitle : navigation.getParam("title", ""),
-            postAuthor : navigation.getParam("author", ""),
-            goldAward : navigation.getParam("gold", ""),
-            silverAward : navigation.getParam("silver", ""),
-            platinumAward : navigation.getParam("platinum", ""),
-            score : navigation.getParam("score", ""),
-            link : navigation.getParam("link", ""),
-            url :  navigation.getParam("url", ""),
-            type : navigation.getParam("type", ""),
-            selfText : navigation.getParam("selftext", ""),
-            nsfw : navigation.getParam("nsfw", "")
-        });
-        //
-        this.fetchSinglePost();
-    }
-    async fetchSinglePost()
-    {
-        var url = this.state.link;
-        var self = this;
-        await axios.get(url)
-        .then(function(response){
-            self.setState({
-                apiData : response.data,
-                apiDataComments : response.data[1].data.children,
-                commentsLoaded : true,
-                isRefreshing : false,
-                
-            });
-        })
-    }
-    renderComments({item, index})
-    {
-        if(index < this.state.apiDataComments.length - 1)
+        constructor(props)
         {
-            return(<View style={styles.commentContainer}>
-                <Text style={styles.commentBodyText}>{item.data.body}</Text>
-                <Text style={styles.commentAuthorText}>{item.data.author}</Text>
-                <View style={styles.commentInfoView} >
-                    <Image width={15} height={15} source={upvoteImg} />
-                    <Text style={styles.commentUpvoteText}>
-                        {item.data.score}
-                    </Text>
-                </View>
-             </View>);
+                super(props);
+                this.state = {
+                        heading : "heading",
+                        content : "content",
+                        apiData : [],
+                        apiDataComments : [],
+                        commentsLoaded : false,
+                        isRefreshing : false,
+                        postTitle : "",
+                        postAuthor : "",
+                        goldAward : 0,
+                        silverAward : 0,
+                        platinumAward : 0,
+                        score : 0,
+                        url : "",
+                        type : "",
+                        link : "",
+                        selftext : "",
+                        nsfw : null,
+                }
+                this.renderComments  = this.renderComments.bind(this);
+                this.fetchSinglePost = this.fetchSinglePost.bind(this);
+                this.renderAcitivityIndicator = this.renderAcitivityIndicator.bind(this);
+                this.refresh = this.refresh.bind(this);
+                this.navigateToUrl = this.navigateToUrl.bind(this);
         }
-        else
+        navigateToUrl(url, type, name)
         {
-            return null;
+                const {navigation} = this.props;
+                this.props.navigation.navigate('webPage',{
+                        url : url,
+                        type : type,
+                        name : name
+                });
         }
-        
-    }
+        async componentWillMount()
+        {
+                const {navigation} = this.props;
+                await this.setState({
+                        postTitle : navigation.getParam("title", ""),
+                        postAuthor : navigation.getParam("author", ""),
+                        goldAward : navigation.getParam("gold", ""),
+                        silverAward : navigation.getParam("silver", ""),
+                        platinumAward : navigation.getParam("platinum", ""),
+                        score : navigation.getParam("score", ""),
+                        link : navigation.getParam("link", ""),
+                        url :  navigation.getParam("url", ""),
+                        type : navigation.getParam("type", ""),
+                        selfText : navigation.getParam("selftext", ""),
+                        nsfw : navigation.getParam("nsfw", "")
+                });
+                //
+                this.fetchSinglePost();
+        }
+        async fetchSinglePost()
+        {
+                var url = this.state.link;
+                var self = this;
+                await axios.get(url)
+                .then(function(response){
+                        self.setState({
+                                apiData : response.data,
+                                apiDataComments : response.data[1].data.children,
+                                commentsLoaded : true,
+                                isRefreshing : false,
+                        });
+                })
+        }
+        renderComments({item, index})
+        {
+                if(index < this.state.apiDataComments.length - 1)
+                {
+                        return(
+                                <View style={styles.commentContainer}>
+                                        <Text style={styles.commentBodyText}>{item.data.body}</Text>
+                                        <Text style={styles.commentAuthorText}>{item.data.author}</Text>
+                                        <View style={styles.commentInfoView} >
+                                                <Image width={15} height={15} source={upvoteImg} />
+                                                <Text style={styles.commentUpvoteText}>
+                                                        {item.data.score}
+                                                </Text>
+                                        </View>
+                                </View>
+                        );
+                }
+                else
+                {
+                return null;
+                }
 
-    renderAcitivityIndicator()
-    {
-        if(!this.state.commentsLoaded)
-        {
-            return(<View style={styles.activityIndicatorStyle}>
-                <ActivityIndicator size="large" color="#0090ab" />
-            </View>);
         }
-       return null;
-    }
-    refresh()
-    {
-        if(!this.state.isRefreshing)
+
+        renderAcitivityIndicator()
         {
-            this.setState({
-                isRefreshing : true,
-            });
-            this.fetchSinglePost();
+                if(!this.state.commentsLoaded)
+                {
+                        return(
+                        <View style={styles.activityIndicatorStyle}>
+                                <ActivityIndicator size="large" color="#0090ab" />
+                        </View>
+                        );
+                }
+                return null;
         }
-        
-    }
-    render()
-    {
-        return(<ScrollView
-            refreshControl={
-                <RefreshControl 
-                  refreshing={this.state.isRefreshing}
-                  onRefresh={this.refresh}
-                  progressBackgroundColor={"#00161A"}
-                  colors={["#0090ab","#0090ab","#00161A"]}
-                />}
-                style={styles.parentContainer}>
-                <StatusBar backgroundColor="#001316"/>
-                <View style={styles.headerBar}>
-                   <Text style={styles.headerBarTitle}>
-                        {this.state.postTitle}
-                   </Text>
-                   <Text style={styles.headerBarUsername}>{this.state.postAuthor}</Text>
-                    <View style={styles.headerBarInfo} >
-                        <Image width={18} height={18} source={upvoteImg} />
-                        <Text style={styles.headerUpvoteText}>
-                            {this.state.score}
-                        </Text>
-                        {this.state.silverAward != null ? (<Text style={styles.silver}>
-                           Silver x{this.state.silverAward}
-                        </Text>):(null)}
-                        {this.state.goldAward != null ? ( <Text style={styles.gold}>
-                           Gold x{this.state.goldAward}
-                        </Text>):(null)}
-                        {this.state.platinumAward != null ? (<Text style={styles.platinum}>
-                           Platinum x{this.state.platinumAward}
-                        </Text>):(null)}      
-                        {this.state.nsfw?(<Text style={styles.nsfw}>NSFW</Text>):(null)}   
-                    </View>
-                </View>
-                <TouchableOpacity onPress={()=>{this.navigateToUrl(this.state.url,this.state.type,this.state.postTitle)}}><Text style={styles.postUrlText}>link : {this.state.url}</Text></TouchableOpacity>
-                {this.state.type == "image"?(<Image width={SCREEN_WIDTH} source={{uri : this.state.url}} />):(null)}
-                {this.state.type == "selftext"?(<Text style={styles.selfText}>{this.state.selfText}</Text>):(null)}
-                <View style={styles.commentHeader}>
-                    <Text style={{color : "white", fontFamily : "Quicksand-Bold"}}>Comments</Text>
-                </View>
-                {this.renderAcitivityIndicator()}
-                <FlatList
-                    keyExtractor={(item, index)=>item.data.id}
-                    data = {this.state.apiDataComments}
-                    renderItem = {this.renderComments}
-                    style = {{paddingBottom : "10%"}}
-                    
-                 />
-            </ScrollView>);
-    }
+        refresh()
+        {
+                if(!this.state.isRefreshing)
+                {
+                        this.setState({
+                        isRefreshing : true,
+                        });
+                        this.fetchSinglePost();
+                }
+
+        }
+        render()
+        {
+                return(
+                        <ScrollView
+                                refreshControl={
+                                <RefreshControl 
+                                refreshing={this.state.isRefreshing}
+                                onRefresh={this.refresh}
+                                progressBackgroundColor={"#00161A"}
+                                colors={["#0090ab","#0090ab","#00161A"]}
+                                />}
+                                style={styles.parentContainer}>
+                                        <StatusBar backgroundColor="#001316"/>
+                                        <View style={styles.headerBar}>
+                                                <Text style={styles.headerBarTitle}>
+                                                        {this.state.postTitle}
+                                                </Text>
+                                                <Text style={styles.headerBarUsername}>{this.state.postAuthor}</Text>
+                                                <View style={styles.headerBarInfo} >
+                                                        <Image width={18} height={18} source={upvoteImg} />
+                                                        <Text style={styles.headerUpvoteText}>
+                                                        {this.state.score}
+                                                        </Text>
+                                                        {this.state.silverAward != null ? (<Text style={styles.silver}>
+                                                        Silver x{this.state.silverAward}
+                                                        </Text>):(null)}
+                                                        {this.state.goldAward != null ? ( <Text style={styles.gold}>
+                                                        Gold x{this.state.goldAward}
+                                                        </Text>):(null)}
+                                                        {this.state.platinumAward != null ? (<Text style={styles.platinum}>
+                                                        Platinum x{this.state.platinumAward}
+                                                        </Text>):(null)}      
+                                                        {this.state.nsfw?(<Text style={styles.nsfw}>NSFW</Text>):(null)}   
+                                                </View>
+                                        </View>
+                                        <TouchableOpacity onPress={()=>{this.navigateToUrl(this.state.url,this.state.type,this.state.postTitle)}}>
+                                                <Text style={styles.postUrlText}>link : {this.state.url}</Text>
+                                        </TouchableOpacity>
+                                        {this.state.type == "image"?(<Image width={SCREEN_WIDTH} source={{uri : this.state.url}} />):(null)}
+                                        {this.state.type == "selftext"?(<Text style={styles.selfText}>{this.state.selfText}</Text>):(null)}
+                                        <View style={styles.commentHeader}>
+                                                <Text style={{color : "white", fontFamily : "Quicksand-Bold"}}>Comments</Text>
+                                        </View>
+                                        {this.renderAcitivityIndicator()}
+                                        <FlatList
+                                        keyExtractor={(item, index)=>item.data.id}
+                                        data = {this.state.apiDataComments}
+                                        renderItem = {this.renderComments}
+                                        style = {{paddingBottom : "10%"}}
+
+                                        />
+                        </ScrollView>
+                );
+        }
 }
 export default ViewPost;
 
